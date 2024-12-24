095c20dc585c08616a09364deb059c4c7c95808e
Maria-bsl <maria@bizlogicsolutions.com>
2024-12-24 11:32:16 +0300

    ...

--- JICHANGEAPI/ClientApp/src/environments/environment.ts
+++ JICHANGEAPI/ClientApp/src/environments/environment.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: false,
-  //baseUrl: 'http://192.168.0.118:90',
+  baseUrl: 'http://183.83.33.156:90',
   //baseUrl: 'http://localhost:62174',
-  baseUrl: 'http://localhost:98',
+  //baseUrl: 'http://192.168.100.50:98',
   //baseUrl: URL, //dynamicUrl
   //isDebug:true,
 };
